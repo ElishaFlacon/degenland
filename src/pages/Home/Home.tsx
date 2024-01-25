e424c329bfc196b39352b74afabdef90795c3c2b
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2024-01-25 21:08:43 +0500

    home page is ready

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,19 +1,24 @@
 import React from "react";
 import { Panel } from "../../components/Panel/Panel";
-import { Button } from "../../components/Button/Button";
+import { ButtonBlue } from "../../components/ButtonBlue/ButtonBlue";
 import "./home.css";
 
 export const Home: React.FC = () => {
     return (
         <Panel>
             <div className='home-bg'>
-                <div className='home-title-block'>
-                    <div className='home-title'>DEGELAND</div>
-                    <div className='home-description'>
-                        Your next country. Real democracy in the web3.
+                <div className='home-content'>
+                    <div className='home-title-block'>
+                        <div className='home-title'>DEGENLAND</div>
+                        <div className='home-description'>
+                            Your next country. Real democracy in the web3.
+                        </div>
                     </div>
+                    <ButtonBlue className='home-button'>
+                        <img src='/images/internet_connection.png' alt='' />
+                        JOIN US
+                    </ButtonBlue>
                 </div>
-                <Button className='home-button'>JOIN US</Button>
             </div>
         </Panel>
     );

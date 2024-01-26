3b4a4925eed4d1228b9511bc9a8158faa902d0e2
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2024-01-26 21:14:33 +0500

    add code page

--- src/pages/Code/Code.tsx
+++ src/pages/Code/Code.tsx
@@ -1,5 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { useFetching } from "../../hooks/useFetching";
+import { CodeInput } from "../../components/CodeInput/CodeInput";
+import { Panel } from "../../components/Panel/Panel";
+import { ButtonBlue } from "../../components/ButtonBlue/ButtonBlue";
+import { TextGreen } from "../../components/TextGreen/TextGreen";
+import "./code.css";
 
 export const Code: React.FC = () => {
-    return <div>Code</div>;
+    const [code, setCode] = useState("");
+
+    const [uploadCode] = useFetching(async () => {
+        // вот сюда запрос к бэку, через аксиос, все есть в директории api
+        console.log(code);
+    });
+
+    const submit = () => {
+        uploadCode();
+    };
+
+    return (
+        <Panel>
+            <div className='code-content'>
+                <TextGreen className='code-title'>Enter your invite code</TextGreen>
+                <CodeInput code={code} setCode={setCode} />
+                <ButtonBlue className='code-button' onClick={submit}>
+                    <img src='/images/internet_connection.png' alt='' />
+                    ENTER
+                </ButtonBlue>
+            </div>
+        </Panel>
+    );
 };

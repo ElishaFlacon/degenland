7b805600ff16c7ada8ea7aeb331b533fdabb094d
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2024-01-26 21:14:01 +0500

    add axios

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,1 +1,18 @@
-export const api = "popa";
+import axios, { AxiosInstance } from "axios";
+
+const API_URL = "";
+
+export const $api: AxiosInstance = axios.create({
+    baseURL: API_URL,
+    withCredentials: true
+});
+
+export const $authorizationApi: AxiosInstance = axios.create({
+    baseURL: API_URL,
+    withCredentials: true
+});
+
+$authorizationApi.interceptors.request.use((config) => {
+    config.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
+    return config;
+});

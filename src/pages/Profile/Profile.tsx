912965c6c59b7f8f72e04d553bc3228e76710983
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2024-01-26 21:59:03 +0500

    add profile page

---
+++ src/pages/Profile/Profile.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Panel } from "../../components/Panel/Panel";
+import "./profile.css";
+
+export const Profile: React.FC = () => {
+    return (
+        <Panel>
+            <div className='profile-content'>Profile</div>
+        </Panel>
+    );
+};
